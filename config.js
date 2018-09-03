16ea83a9bd76ead2a8646f86758addcd32b86eb6
Rajesh Botchu <rajessrao@gmail.com>
2018-09-03 22:12:28 +0530

    Added mlab dB

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 module.exports = {
 
     'secret': 'Trident@#$2018',
+    // mlab Database
+    // 'database': 'mongodb://testuser:testuser%40123@ds020938.mlab.com:20938/dbfortest',
+    // local Database
     'database': 'mongodb://localhost:27017/test1',
     'apiEndpoint': 'https://api.iotworldlabs.io/iirp/?guid=d97a3ae7-8f2f-4789-9f61-62b5a2ef6ce5'
 
